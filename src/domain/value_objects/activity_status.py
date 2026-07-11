aea67c2edb9752d4ba57ecaa72b744dd049a0bdc
Pavel Okhrim <paokhrim@gmail.com>
2026-07-11 07:47:57 +0300

    Add activity status value object

---
+++ src/domain/value_objects/activity_status.py
@@ -0,0 +1,11 @@
+"""Processing activity lifecycle states."""
+
+from enum import StrEnum, auto
+
+
+class ActivityStatus(StrEnum):
+    DRAFT = auto()
+    ACTIVE = auto()
+    UNDER_REVIEW = auto()
+    SUSPENDED = auto()
+    ARCHIVED = auto()
